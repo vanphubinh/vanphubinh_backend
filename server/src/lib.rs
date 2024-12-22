0f025d5dbf626802b6c9802deb9b37ab42c9270e
An <an.tran511@outlook.com>
2024-12-22 22:43:13 +0700

    feat: add tracing to app

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,23 +1,37 @@
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use std::net::SocketAddr;
-
-use axum::{routing::get, Router};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 pub async fn start() {
-  let app = Router::new().route("/", get(root));
+  tracing_subscriber::fmt::init();
+
+  let app = Router::new()
+    .route("/", get(root))
+    .route("/complex", get(complex));
 
   let port: u16 = std::env::var("PORT")
-    .unwrap_or_else(|_| "3000".to_string())
+    .unwrap_or("3000".into())
     .parse()
-    .expect("Failed to parse PORT");
+    .expect("failed to convert to number");
 
-  let address = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], port));
-  let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+  let ipv6 = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], port));
+  let ipv6_listener = TcpListener::bind(&ipv6).await.unwrap();
 
-  axum::serve(listener, app).await.unwrap();
+  tracing::info!("Listening on IPv6 at {}!", ipv6);
+
+  axum::serve(ipv6_listener, app).await.unwrap();
 }
 
-// basic handler that responds with a static string
 async fn root() -> &'static str {
-  "Hello World, from Axum!"
+  "Hello, World!"
+}
+
+async fn complex() -> impl IntoResponse {
+  (
+    StatusCode::OK,
+    Json(serde_json::json!({
+        "message": "Hello, World!"
+    })),
+  )
 }
