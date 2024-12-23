232af47b256d0591a7f48152093c4eac92cbc8b8
An <an.tran511@outlook.com>
2024-12-23 10:51:53 +0700

    feat: config to connection to database

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,5 +1,8 @@
+use std::{net::SocketAddr, sync::Arc};
+
 use axum::Router;
-use std::net::SocketAddr;
+use infra::state::AppState;
+use sea_orm::{Database, DatabaseConnection, DbErr};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -8,12 +11,27 @@ pub async fn start() {
 
   tracing_subscriber::fmt::init();
 
-  let app = Router::new().merge(interface::new());
+  tracing::info!("Connecting to database...");
+
+  let db = match connect_db("DATABASE_URL").await {
+    Ok(db) => {
+      tracing::info!("Connected to database!");
+      db
+    }
+    Err(_) => {
+      tracing::error!("Failed to connect to database!");
+      return;
+    }
+  };
+
+  let app_state = Arc::new(AppState::new(db));
+
+  let app = Router::new().with_state(app_state.clone());
 
   let port: u16 = std::env::var("PORT")
     .unwrap_or("3000".into())
     .parse()
-    .expect("failed to convert to number");
+    .expect("Failed to convert to number");
 
   let ipv6 = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], port));
   let ipv6_listener = TcpListener::bind(&ipv6).await.unwrap();
@@ -21,4 +39,10 @@ pub async fn start() {
   tracing::info!("Listening on IPv6 at {}!", ipv6);
 
   axum::serve(ipv6_listener, app).await.unwrap();
+}
+
+async fn connect_db(env_var: &str) -> Result<DatabaseConnection, DbErr> {
+  let db_url = std::env::var(env_var).unwrap();
+  let db = Database::connect(&db_url).await?;
+  Ok(db)
 }
